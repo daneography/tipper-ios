cc916456644e2bd72553e0ab723ee9f99b807a37
Dane Acena <acena.dane@gmail.com>
2020-07-27 20:59:10 -0400

    xcode project start

---
+++ tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Dane Acena on 7/27/20.
+//  Copyright © 2020 daneography. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
